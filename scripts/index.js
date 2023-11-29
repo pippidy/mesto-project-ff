16a11e2974e7814bd676109e11599d681b7fc1a9
Andrey Koshelev <pippidy@gmail.com>
2023-11-29 12:38:13 +0400

    Final script

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,24 @@
 const container = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
-const cardImage = cardTemplate.querySelector('.card__image');
-const cardName = cardTemplate.querySelector('.card__title');
-console.log(cardName)
 
-// @todo: DOM узлы
+function makeCard(name, imgLink, removeCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardName = card.querySelector('.card__title');
+    const cardImg = card.querySelector('.card__image');
+    const buttonRemove = card.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+    cardName.textContent = name;
+    cardImg.src = imgLink;
 
-// @todo: Функция удаления карточки
+    buttonRemove.addEventListener('click', removeCard);
 
-// @todo: Вывести карточки на страницу
+    return card;
+}
+
+function removeCard(event) {
+    event.target.closest('.card').remove();
+}
+
+initialCards.forEach(function (item) {
+    container.append(makeCard(item.name, item.link, removeCard));
+});
