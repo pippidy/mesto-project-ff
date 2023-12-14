da024ae8092727c2cdf46f0101215e44e771ebec
Andrey Koshelev <pippidy@gmail.com>
2023-12-14 16:06:02 +0400

    Webpack

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+const preset = [
+    ['babel-env', {
+        targets: {
+            edge: '17',
+            ie: '11',
+            firefox: '50',
+            chrome: '64',
+            safari: '11.1'
+        },
+        useBuiltIns: 'entry'
+    }]
+];
